c8c11b8676692ed14a45b42aaa78c6f455e99a80
Edmon Grigoryan <edmon.grigoryan23@gmail.com>
2022-01-26 02:01:57 +0400

    sa function and some functions connected to linked list

--- Utils.c
+++ Utils.c
@@ -1,9 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct Node {
     int data;
     struct Node* next;
 }Stack;
 
-void sa(Stack** head)
+void sa(Stack* head)
+{
+    while (head->next != NULL)
+    {
+        if (head->next->next->next = NULL)
+        {
+            
+            Stack* temp = (Stack*)malloc(sizeof(Stack*));
+            head->next = head->next->next->next;
+            temp = head->next->next;
+            head->next->next = NULL;
+            head->next->next->next = head->next->next;
+        }
+        head = head->next;
+    }
+}
+
+void printList(Stack* head)
+{
+    while(head != NULL)
+    {
+        printf("%d", head->data);
+        head = head->next;
+    }
+}
+
+void addNode(Stack* head,int num)
+{
+    Stack* new = (Stack*)malloc(sizeof(Stack*));
+    new->data = num;
+    new->next = head;
+    printf("%d",new->data);
+    head = new;
+}
+
+int main(void)
 {
     
+    Stack* head = NULL;
+    int i = 0;
+    while(i < 5)
+    {
+        
+        addNode(head,i);
+        i++;
+    }
+
+    printList(head);
 }
