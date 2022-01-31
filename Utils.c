55d12c4b7c4833f0abfe4e6e4625f2b8ab2b5175
Edmon Grigoryan <edmon.grigoryan23@gmail.com>
2022-01-31 22:25:41 +0400

    sa sb pa pb

--- Utils.c
+++ Utils.c
@@ -4,37 +4,90 @@
 typedef struct Node {
     int data;
     struct Node* next;
-}Stack;
+}stack;
 
-void sa(Stack** head)
+
+void push(stack** head, stack** pushed)
 {
-    Stack** current = head;
-    while ((*current)->next != NULL)
+    (*pushed)->next = *head;
+    *head = *pushed;
+}
+
+
+
+void pop(stack** head, stack** poped)
+{
+    stack* new = *head;
+    *poped = new;
+    *head = (*head)->next;
+    new->next = NULL;
+}
+
+void sasb(stack** swaped, char c)
+{
+    stack* first = NULL;
+    stack* second = NULL;
+    pop(swaped,&first);
+    pop(swaped,&second);
+    push(swaped,&first);
+    push(swaped,&second);
+    if(c == 'a')
+        printf("sa");
+    else printf("sb");
+}
+
+void ss(stack** a, stack** b)
+{
+    stack* firsta = NULL;
+    stack* seconda = NULL;
+    pop(a,&firsta);
+    pop(a,&seconda);
+    push(a,&firsta);
+    push(a,&seconda);
+
+    stack* firstb = NULL;
+    stack* secondb = NULL;
+    pop(b,&firsta);
+    pop(b,&seconda);
+    push(b,&firsta);
+    push(b,&seconda);
+
+    printf("ss");
+}
+
+void papb(stack** a, stack** b, char c)
+{
+    stack* poped = NULL;
+    if(c == a)
     {
-        if ((*current)->next->next->next->next = NULL)
-        {
-            
-            // Stack* temp = (Stack*)malloc(sizeof(Stack*));
-            // temp = (*head)->next->next;
-
-            printf("%d\n",(*current)->next->data );
-            printf("%d\n",(*current)->next->next->data );
-            // printf("%d\n",(*head)->next->next->next->data );
-            // (*head)->next = (*head)->next->next->next;
-            // (*head)->next->next->next->next = temp;
-            // temp->next = NULL;
-
-
-
-            // (*head)->next->next->next = NULL;
-            // (*head)->next->next->next->next = (*head)->next->next;
-            break;
-        }
-        (*current)= (*current)->next;
+        pop(a,&poped);
+        push(b,&poped);
+        printf("pa");
+    }
+    else
+    {
+        pop(b,&poped);
+        push(a,&poped);
+        printf("pb");
     }
 }
 
-void printList(Stack* head)
+    
+void sasb(stack** swaped, char c)
+{
+    stack* first = NULL;
+    stack* second = NULL;
+    pop(swaped,&first);
+    pop(swaped,&second);
+    push(swaped,&first);
+    push(swaped,&second);
+    if(c == 'a')
+        printf("sa");
+    else printf("sb");
+
+}
+
+void printList(stack* head)
 {
     while(head != NULL)
     {
@@ -44,18 +97,21 @@ void printList(Stack* head)
     printf("\n");
 }
 
-void addNode(Stack** head,int num)
+void addNode(stack** head,int num)
 {
-    Stack* new = (Stack*)malloc(sizeof(Stack*));
+    stack* new = (stack*)malloc(sizeof(stack*));
     new->data = num;
     new->next = *head;
     *head = new;
     
 }
 
+
 int main(void)
 {
-    Stack* head = NULL;
+    stack* head = NULL;
+    stack* poped = NULL;
+    stack* pushed = NULL;
     int i = 0;
     while(i < 10)
     { 
@@ -63,10 +119,13 @@ int main(void)
         // printf("%d",head->data);
         i++;
     }
+    printList(head);
+    sa(&head);
     printf("\n");
     printList(head);
-    
+    // test(&head,&sb);
+    // printList(head);
     // printf("%d\n",head->next->next->next->next->next->data);
-    sa(&head);
-    printList(head);
+    // sa(&head);
+    // printList(head);
 }
